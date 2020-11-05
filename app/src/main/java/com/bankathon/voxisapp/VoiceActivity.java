e2c46a40f0da2a5cb718b3d1ae1ee2ca5e1a68eb
fc-parag <parag.vyas@freecharge.com>
2020-11-05 21:09:01 +0530

    mic check

--- app/src/main/java/com/bankathon/voxisapp/VoiceActivity.java
+++ app/src/main/java/com/bankathon/voxisapp/VoiceActivity.java
@@ -6,13 +6,17 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -21,8 +25,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Locale;
+import java.util.UUID;
 
 public class VoiceActivity extends AppCompatActivity {
     public static final Integer RecordAudioRequestCode = 1;
@@ -30,94 +37,77 @@ public class VoiceActivity extends AppCompatActivity {
     private EditText editText;
     private ImageView micButton;
 
+    private String fileName;
+    private MediaRecorder recorder;
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice);
+
+
         if(ContextCompat.checkSelfPermission(this,Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
             checkPermission();
         }
 
         editText = findViewById(R.id.text_voice);
         micButton = findViewById(R.id.iv_mic);
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
 
-        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-
-        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+        micButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onReadyForSpeech(Bundle bundle) {
-
-            }
-
-            @Override
-            public void onBeginningOfSpeech() {
-                editText.setText("");
-                editText.setHint("Listening...");
-            }
-
-            @Override
-            public void onRmsChanged(float v) {
-
-            }
-
-            @Override
-            public void onBufferReceived(byte[] bytes) {
-
-            }
-
-            @Override
-            public void onEndOfSpeech() {
-
-            }
-
-            @Override
-            public void onError(int i) {
-
-            }
-
-            @Override
-            public void onResults(Bundle bundle) {
-                micButton.setImageResource(R.drawable.logo_1);
-                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                editText.setText(data.get(0));
-            }
-
-            @Override
-            public void onPartialResults(Bundle bundle) {
-
-            }
-
-            @Override
-            public void onEvent(int i, Bundle bundle) {
-
+            public void onClick(View v) {
+                startRecording();
             }
         });
 
-        micButton.setOnTouchListener(new View.OnTouchListener() {
+        ImageView stopRecordingButton = findViewById(R.id.iv_mic_1);
+        stopRecordingButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_UP){
-                    speechRecognizer.stopListening();
-                }
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
-                    micButton.setImageResource(R.drawable.logo_2);
-                    speechRecognizer.startListening(speechRecognizerIntent);
-                }
-                return false;
+            public void onClick(View view) {
+                stopRecording();
             }
         });
 
-
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        speechRecognizer.destroy();
+    private void stopRecording() {
+        String uuid = UUID.randomUUID().toString();
+        fileName = getCacheDir1();
+        Log.i(MainActivity.class.getSimpleName(), fileName);
+
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(fileName);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            Log.e(MainActivity.class.getSimpleName() + ":startRecording()", "prepare() failed");
+        }
+
+        recorder.start();
     }
+
+    private void startRecording() {
+        if (recorder != null) {
+            recorder.release();
+            recorder = null;
+        }
+    }
+
+
+    public String getCacheDir1() {
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/voxisDir";
+        File storageDir = new File(path);
+        if (!storageDir.exists()) {
+            storageDir.mkdirs();
+            // This should never happen - log handled exception!
+        }
+        return path + "/aud-" + System.currentTimeMillis() + ".wav";
+    }
+
 
     private void checkPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
